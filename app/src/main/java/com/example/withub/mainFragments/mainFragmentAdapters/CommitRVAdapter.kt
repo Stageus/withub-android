03fd0c9f2df5ad7aea3f8f426937a66e7c5b8225
want8607 <84075111+want8607@users.noreply.github.com>
2022-08-02 21:50:57 +0900

    UPDATE CommitRVAdadapter Holder inner class was renamed with CommitHolder HolderClass doesn't have the function anymore(bind) All binding process will be worked by onBindViewHolder BindingView was added to CommitRVAdapter

--- app/src/main/java/com/example/withub/mainFragments/mainFragmentAdapters/CommitRVAdapter.kt
+++ app/src/main/java/com/example/withub/mainFragments/mainFragmentAdapters/CommitRVAdapter.kt
@@ -1,54 +1,45 @@
 package com.example.withub.mainFragments.mainFragmentAdapters
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.withub.CommitData
-import com.example.withub.R
+import com.example.withub.databinding.RecyclerViewItemCommitBinding
 
-class CommitRVAdapter(val context : Context, var list : List<CommitData>) : RecyclerView.Adapter<CommitRVAdapter.Holder>(){
+class CommitRVAdapter(val context : Context, var list : List<CommitData>) : RecyclerView.Adapter<CommitRVAdapter.CommitHolder>(){
 
-    inner class Holder(itemView: View?):RecyclerView.ViewHolder(itemView!!){
+    inner class CommitHolder(val binding:RecyclerViewItemCommitBinding):RecyclerView.ViewHolder(binding.root)
 
-        var dateTextview = itemView?.findViewById<TextView>(R.id.commit_date_text_view)
-        var repositoryName = itemView?.findViewById<TextView>(R.id.commit_repository_name)
-        var commitMessage = itemView?.findViewById<TextView>(R.id.commit_message_text_view)
-        var commitClickArea = itemView?.findViewById<ConstraintLayout>(R.id.commit_constraint_layout)
-        fun bind(position: Int){
-            dateTextview?.text = list[position].date +" "+ list[position].time
-            repositoryName?.text = list[position].repository
-            commitMessage?.text = list[position].commit_message
-            commitClickArea?.setOnClickListener {
-                val intent = Intent(Intent.ACTION_VIEW,
-                    Uri.parse("https://github.com/${list[position].repository}/commit/${list[position].sha}"))
-                context.startActivity(intent)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommitHolder {
+        val binding = RecyclerViewItemCommitBinding
+                    .inflate(LayoutInflater.from(parent.context),parent,false)
+        return CommitHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CommitHolder, position: Int) {
+        holder.apply{
+            list[position].apply{
+                binding.commitDateTextView.text = this.date +" "+ this.time
+                binding.commitRepositoryName.text = this.repository
+                binding.commitMessageTextView.text = this.commit_message
+                binding.commitConstraintLayout.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://github.com/${this.repository}/commit/${this.sha}"))
+                    context.startActivity(intent)
+                }
             }
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommitRVAdapter.Holder {
-        val view = LayoutInflater.from(context).inflate(R.layout.`recycler_view_item_commit.xml`, parent, false)
-        return Holder(view)
-    }
+    override fun getItemCount(): Int = list.size
 
-    override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder.bind(position)
-    }
-
-    override fun getItemCount(): Int {
-        return list.size
-    }
-
-    @SuppressLint("NotifyDataSetChanged")
     fun updateDataset(newList : List<CommitData>){
         list = newList
         notifyDataSetChanged()
     }
+
+
 }
